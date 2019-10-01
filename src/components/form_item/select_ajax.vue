24a131006d7a913d85f6d24c44549b443a02d6a4
lyh <113381161@qq.com>
2019-10-01 17:48:52 +0800

    lyh

--- src/components/form_item/select_ajax.vue
+++ src/components/form_item/select_ajax.vue
@@ -1,5 +1,13 @@
 <template>
-  <el-select :multiple="multiple"  collapse-tags v-model="valueNeed" placeholder="请选择" @change="change" clearable filterable>
+  <el-select
+    :multiple="multiple"
+    collapse-tags
+    v-model="valueNeed"
+    placeholder="请选择"
+    @change="change"
+    clearable
+    filterable
+  >
     <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
   </el-select>
 </template>
@@ -9,9 +17,9 @@ export default {
   props: {
     multiple: {
       default: false,
-      type:Boolean,
+      type: Boolean
     },
-    value: [String, Number,Array],
+    value: [String, Number, Array],
     ajaxUrl: {
       default: "/crossList?page=mabang-category"
     },
@@ -21,7 +29,17 @@ export default {
     keyLabel: {
       default: "name"
     },
-    param: [Object],
+    param: [Object]
+  },
+  watch: {
+    param: {
+      handler(newVal, oldVal) {
+        console.log("param changed###############");
+        this.ajaxGetOptions(); //调用：{ajax获取选项函数}
+      },
+
+      deep: true
+    }
   },
   data() {
     return {
@@ -37,26 +55,31 @@ export default {
   methods: {
     change() {
       this.$emit("input", this.valueNeed); //触发双向绑定
+    },
+    //函数：{ajax获取选项函数}
+    async ajaxGetOptions() {
+      console.log("ajaxGetOptions");
+      let { data } = await axios({
+        //请求接口
+        method: "post",
+        url: PUB.domain + this.ajaxUrl,
+        data: this.param //传递参数
+      }).catch(function(error) {
+        alert("异常:" + error);
+      });
+      let list = data.list;
+
+      this.options = list.map(item => {
+        //将list数组转换成options的结构
+        return {
+          value: item[this.keyValue],
+          label: item[this.keyLabel]
+        };
+      });
     }
   },
   async created() {
-    let { data } = await axios({
-      //请求接口
-      method: "post",
-      url: PUB.domain+this.ajaxUrl,
-      data: this.param //传递参数
-    }).catch(function(error) {
-      alert("异常:" + error);
-    });
-    let list = data.list;
-
-    this.options = list.map(item => {
-      //将list数组转换成options的结构
-      return {
-        value: item[this.keyValue],
-        label: item[this.keyLabel]
-      };
-    });
+    this.ajaxGetOptions(); //调用：{ajax获取选项函数}
   }
 };
 </script>
